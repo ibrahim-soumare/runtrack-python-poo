51e1403c1d8330ef93f134f9addd0186a39177e2
ibrahim-soumare <ibrahim.soumare@laplateforme.io>
2024-01-03 15:38:33 +0100

    job01

---
+++ jour01/job01.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+operation_instance = Operation(12, 29)
+
+print(operation_instance)
